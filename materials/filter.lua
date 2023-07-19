3d5f4784af8fe1efcba5cd85c713c2fb3f3853e2
Matt Farrow <mattfarrow1@gmail.com>
2023-07-18 21:09:04 -0500

    update slides

--- materials/filter.lua
+++ materials/filter.lua
@@ -1,7 +1,7 @@
 function Header (el)
   -- I am using level 2 header for slides in the markdown below
   if el.level == 2 then
-    el.attributes['background-image'] = '/slide templates/Slide7.png'
+    el.attributes['background-image'] = '/materials/slide templates/Slide7.png'
     el.attributes['background-size'] = 'contain'
   end
   return el
